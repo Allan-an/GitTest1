6d87c6ac1ca8ba34bbff88ae1536b86bd2baef8f
Allan-an <32536062+Allan-an@users.noreply.github.com>
2017-11-17 11:17:38 +0530

    Update Feature1.cs

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -10,10 +10,10 @@ namespace ConsoleApp1
     {
         public int Add()
         {
-            var x1 = 1;
-            var x2 = 2;
-            var sum = x1 + x2;
-            return sum;
+           int x1 = 1;
+           int x2 = 2;
+           int sum = x1 + x2;
+           return sum;
         }
     }
 }
